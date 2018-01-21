bf0509d22deec40b7ca0f2dbe5d6797bcfa9a9fe
NS <NS@DESKTOP-BLG09RO>
2018-01-21 22:58:41 +0300

    add new class

--- Ahmedsafaa/Form1.cs
+++ Ahmedsafaa/Form1.cs
@@ -16,5 +16,10 @@ namespace Ahmedsafaa
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ahmedsafaa" +"Mohmed aldbasss");
+        }
     }
 }
